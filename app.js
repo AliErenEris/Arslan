e0478907cd6bd9c687e24eaf0a3d93b6cb0fbeef
Ali Eren Eriş <83022034+AliErenEris@users.noreply.github.com>
2022-10-19 22:44:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const middleware = function (req, res, next) {
+  console.log("yeni bir istek gönderildi");
+  next();
+};
+
+const middleware2 = function (req, res, next) {
+  console.log("Post isteği için istek gönderildi");
+  next();
+};
+
+
+
+app.get("/", middleware, function (req, res) {
+  console.log("Merhaba, GET isteği attınız");
+  res.json("Nodejs Restapi Basic Example");
+});
+
+app.put("/", middleware, function (req, res) {
+  console.log("Merhaba, PUT isteği attınız");
+  res.json("Nodejs Restapi Basic Example");
+});
+
+app.post("/", middleware, middleware2, function (req, res) {
+  console.log("Merhaba, POST isteği attınız");
+  res.json("Nodejs Restapi Basic Example");
+});
+
+app.delete("/", middleware, function (req, res) {
+  console.log("Merhaba, delete isteği attınız");
+  res.json("Nodejs Restapi Basic Example");
+});
+
+app.listen(PORT, () => {
+  console.log("Ready on http://localhost:" + PORT);
+});
